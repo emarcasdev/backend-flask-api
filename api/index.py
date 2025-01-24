250187dc380cf6bf051cd85b8b9fbe072d2b6a8b
emarcasdev <emarcasdev@gmail.com>
2025-01-24 18:13:15 +0100

    Solucción para el funcionamiento de MongoDB, excluir el _id 😎

--- api/index.py
+++ api/index.py
@@ -1,15 +1,22 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 
-users = [
-    {"id": 1, "nombre": "Eder", "apellido": "Martínez", "tlfn": 657543209},
-    {"id": 2, "nombre": "David", "apellido": "Puga", "tlfn": 657543209},
-    {"id": 3, "nombre": "Diego", "apellido": "Perez", "tlfn": 657543209},
-    {"id": 4, "nombre": "Agus", "apellido": "Alonso", "tlfn": 657543209}
-]
+# Conexión a MongoDB
+client = MongoClient("mongodb+srv://emarcasdev:Lgv5EiO0N1RAxRiX@emarcasdev.hlq6d.mongodb.net/?retryWrites=true&w=majority&appName=emarcasdev") 
+db = client["express_back"]
+users_collection = db["users"]
+
+# Prueba anterior
+# users = [
+#     {"id": 1, "nombre": "Eder", "apellido": "Martínez", "tlfn": 657543209},
+#     {"id": 2, "nombre": "David", "apellido": "Puga", "tlfn": 657543209},
+#     {"id": 3, "nombre": "Diego", "apellido": "Perez", "tlfn": 657543209},
+#     {"id": 4, "nombre": "Agus", "apellido": "Alonso", "tlfn": 657543209}
+# ]
 
 @app.route('/')
 def home():
@@ -21,18 +28,38 @@ def about():
 
 @app.route('/api/users', methods=["GET"])
 def get_users():
+    users = list(users_collection.find({}, {"_id": 0}))
     return jsonify(users)
 
 @app.route('/api/users', methods=["POST"])
 def add_user():
-    new_user = request.get_json()
-    users.append(new_user)
-    return jsonify(new_user), 201
+    # Recuperamos los datos necesarios del front
+    data = request.get_json()
+    nombre = data.get("nombre")
+    apellido = data.get("apellido")
+    telefono = data.get("telefono")
+
+    try:
+        # Contamos la cantidad de documentos existentes en la colección
+        user_count = users_collection.count_documents({},)
+        # Creamos el nuevo usuario con el id incrementado
+        new_user = {
+            "id": user_count + 1,
+            "nombre": nombre,
+            "apellido": apellido,
+            "telefono": telefono
+        }
+        # Insertamos el nuevo usuario en la colección
+        result = users_collection.insert_one(new_user)
+        # Devolvemos el ID del nuevo documento insertado
+        return jsonify({"insertedId": str(result.inserted_id)}), 201
+    except Exception as e:
+        print("Error al agregar el usuario:", e)
+        return jsonify({"error": "Error al agregar el usuario"}), 500
 
 @app.route('/api/user1', methods=["GET"])
 def get_user_one():
-    user_id = 1  
-    user = next((user for user in users if user["id"] == user_id), False)
+    user = users_collection.find_one({"id": 1}, {"_id": 0})
 
     if user:
         return jsonify(user)  
@@ -42,7 +69,7 @@ def get_user_one():
 @app.route('/api/user', methods=["GET"])
 def get_user_by_id():
     user_id = request.args.get("id", type=int)
-    user = next((user for user in users if user["id"] == user_id), False)
+    user = users_collection.find_one({"id": user_id}, {"_id": 0})
 
     if user:
         return jsonify(user)
